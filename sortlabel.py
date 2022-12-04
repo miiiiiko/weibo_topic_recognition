382904a332c6cd8ecbfa10885a69e8d271a592fe
HenrynsXu <116343602+HenrynsXu@users.noreply.github.com>
2022-12-04 21:22:14 +0800

    更新：可以得到频数小于等于x和大于x的标签的分界点，[:x]得到大于x的，[x:]得到小于等于x的

--- sortlabel.py
+++ sortlabel.py
@@ -9,19 +9,21 @@ class TokenList:
             for i, xx in enumerate(special_marks): tdict[xx] = 100000000 - i
             for xx in source:
                 for token in func(xx): tdict[token] += 1
-            tokens = FreqDict2List(tdict)  # already up sorted
+            tokens = FreqDict2List(tdict)  # already decline sorted
             tokens = [x for x in tokens if x[1] >= save_low_freq]
             SaveCSV(tokens, file)
         self.id2t = [ '<UNK>'] + [x for x,y in LoadCSV(file) if float(y) >= low_freq]
         self.t2id = {v:k for k,v in enumerate(self.id2t)}
-    def get_id(self, token): return self.t2id.get(token, 1)
+        self.id2freq = [int(y) for x,y in LoadCSV(file) if float(y) >= low_freq]
+        self.freq2id = {v:k+1 for k,v in enumerate(self.id2freq)}
+    def get_id(self, token): return self.t2id.get(token, -1)
     def get_token(self, ii): return self.id2t[ii]
     def get_num(self): return len(self.id2t)
 savefile='sortlabel.txt'
 sourcefile='train.json'
 f = lambda x:x['label']
-llist = TokenList(file=savefile,source=LoadJsons(sourcefile),func=f)
-def label2vec(targrtlabels:list,dims= 1400):
+llist = TokenList(file=savefile,source=LoadJsons(sourcefile),func=f,low_freq=1,save_low_freq=1)
+def label2vec(targrtlabels:list,dims= llist.get_num()):
     lab_vec = numpy.zeros(dims)
     for label in targrtlabels:
         loc = llist.get_id(label)
@@ -33,3 +35,9 @@ def label2id(targrtlabels:list):
     for label in targrtlabels:
         locs.append(llist.get_id(label))
     return locs
+def loc(num):
+    for inx,i in enumerate(llist.id2freq): 
+        if i < num+1: return inx+1
+        elif inx == len(llist.id2freq)-1: return inx+1
+def sep_point(freq_list:list):
+    return [loc(item) for item in freq_list]
