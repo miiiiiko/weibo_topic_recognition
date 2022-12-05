149a8206632e10cf7eb3bf34fdeaa0a6ae3a4312
HenrynsXu <116343602+HenrynsXu@users.noreply.github.com>
2022-12-05 21:23:56 +0800

    增加注释，然后需要在主程序中自行建立llist

--- sortlabel.py
+++ sortlabel.py
@@ -19,23 +19,23 @@ class TokenList:
     def get_id(self, token): return self.t2id.get(token, -1)
     def get_token(self, ii): return self.id2t[ii]
     def get_num(self): return len(self.id2t)
-savefile='sortlabel.txt'
-sourcefile='train.json'
-f = lambda x:x['label']
-llist = TokenList(file=savefile,source=LoadJsons(sourcefile),func=f,low_freq=1,save_low_freq=1)
-def label2vec(targrtlabels:list,dims= llist.get_num()):
-    lab_vec = numpy.zeros(dims)
+
+def label2vec(targrtlabels:list,llist:TokenList):
+    lab_vec = numpy.zeros(llist.get_num())
     for label in targrtlabels:
         loc = llist.get_id(label)
         lab_vec[loc] = 1.0
     return lab_vec
 
-def label2id(targrtlabels:list):
+def label2id(targrtlabels:list,llist:TokenList):
     locs = []
     for label in targrtlabels:
         locs.append(llist.get_id(label))
     return locs
-def loc(num):
+def loc(num:int, llist:TokenList):
+    '''
+    返回llist中第一个频数为num的id，llist[:x]得到的是频数大于num的列表，[x:]则为小于等于
+    '''
     for inx,i in enumerate(llist.id2freq): 
         if i < num+1: return inx+1
         elif inx == len(llist.id2freq)-1: return inx+1
