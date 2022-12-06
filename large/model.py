75cdeb75ab9e04c2cd995999a06a2305d7078839
HenrynsXu <116343602+HenrynsXu@users.noreply.github.com>
2022-12-06 22:03:44 +0800

    模型，wwm-ext-large就用model_large

---
+++ large/model.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from transformers import BertTokenizer,BertModel
+
+class MLP(nn.Module):
+    def __init__(self, n_in, n_out): # dropout=0
+        super().__init__()
+
+        self.linear = nn.Linear(n_in, n_out)
+        self.activation = nn.Sigmoid()
+        # self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # x = self.dropout(x)
+        x = self.linear(x)
+        x = self.activation(x)
+        return x
+
+
+class Model(nn.Module):
+    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext',n=1400):
+        super(Model, self).__init__()
+        self.encoder = BertModel.from_pretrained(model_path)
+        self.pred = MLP(768,n)
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+
+    def forward(self,input_ids):
+        x = self.encoder(input_ids).pooler_output
+        x = self.pred(x)
+        return x
+
+class Model_large(nn.Module):
+    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext-large',n=1400):
+        super(Model_large, self).__init__()
+        self.encoder = BertModel.from_pretrained(model_path)
+        self.pred = MLP(1024,n)
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+
+    def forward(self,input_ids):
+        x = self.encoder(input_ids).pooler_output
+        x = self.pred(x)
+        return x
