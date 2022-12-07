d3aa439fa393ad87555759edc21321f282480d5b
miiiiiko <97025329+miiiiiko@users.noreply.github.com>
2022-12-07 21:41:41 +0800

    Update model.py

--- base1/model.py
+++ base1/model.py
@@ -22,16 +22,21 @@ class MLP(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext',n=1400):
+    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext',n=1400,encoder_type = 'cls'):
         super(Model, self).__init__()
         self.encoder = BertModel.from_pretrained(model_path)
         self.pred = MLP(768,n)
+        self.encoder_type = encoder_type
         # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
 
     # def forward(self,input_ids,attention_mask,token_type_ids):
     def forward(self,inputs):
-        x = self.encoder(inputs).pooler_output
+        x = self.encoder(inputs)
+        if self.encoder_type == "cls":
+            x = x.last_hidden_state[:,0]
+        if self.encoder_type == "pooler":
+            x = x.pooler_output
         x = self.pred(x)
         return x
 
