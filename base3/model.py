3f4c9089126db26ed83a5f305c0574beea5ca092
miiiiiko <97025329+miiiiiko@users.noreply.github.com>
2022-12-07 21:37:30 +0800

    Add files via upload

--- base3/model.py
+++ base3/model.py
@@ -22,30 +22,41 @@ class MLP(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext',n=2):
+    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext',n=2, encoder_type = 'cls'):
         super(Model, self).__init__()
         self.encoder = BertModel.from_pretrained(model_path)
+        self.encoder_type = encoder_type
         self.pred = MLP(768,n)
         # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
 
     def forward(self,input_ids,attention_mask,token_type_ids):
     # def forward(self,inputs):
-        x = self.encoder(input_ids,token_type_ids = token_type_ids,attention_mask = attention_mask).pooler_output
+        x = self.encoder(input_ids,token_type_ids = token_type_ids,attention_mask = attention_mask)
+        if self.encoder_type == "cls":
+            x = x.last_hidden_state[:,0]
+        if self.encoder_type == "pooler":
+            x = x.pooler_output
+        
         x = self.pred(x)
         return x
 
 
 class Model_l(nn.Module):
-    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext-large',n=1400):
+    def __init__(self, model_path='hfl/chinese-roberta-wwm-ext-large',n=1400,encoder_type = 'cls'):
         super(Model_l, self).__init__()
         self.encoder = BertModel.from_pretrained(model_path)
         self.pred = MLP(1024,n)
+        self.encoder_type = encoder_type
         # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
 
     def forward(self,input_ids,attention_mask,token_type_ids,):
     # def forward(self,inputs):
-        x = self.encoder(input_ids,token_type_ids = token_type_ids,attention_mask = attention_mask).pooler_output
+        x = self.encoder(input_ids,token_type_ids = token_type_ids,attention_mask = attention_mask)
+        if self.encoder_type == "cls":
+            x = x.last_hidden_state[:,0]
+        if self.encoder_type == "pooler":
+            x = x.pooler_output
         x = self.pred(x)
         return x
