b09da15a3f1d0e9f7c49ab9a9d3ed2c809e44496
miiiiiko <97025329+miiiiiko@users.noreply.github.com>
2022-12-11 20:31:08 +0800

    Add files via upload

--- base5/main.py
+++ base5/main.py
@@ -13,17 +13,14 @@ from transformers import BertTokenizer
 from model import Model
 import torch.nn as nn
 from torch.optim import AdamW
-import torch.nn.functional as F
 import transformers
 from tqdm import tqdm
-import math
+import torch.nn.functional as F
 from sklearn import metrics
 import time
 import ljqpy
 import pt_utils
 from ljqpy import LoadJsons
-from pairdata import StcPairSet,pairs_vs,sentence_pair_gen
-
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 model_name = 'hfl/chinese-roberta-wwm-ext'
@@ -36,9 +33,9 @@ def load_data(fn):
     # print('loading data')
     return [(x["text_normd"], x["label"]) for x in ljqpy.LoadJsons(fn)]
 
-# datadir = './dataset'
+datadir = './dataset'
 
-# xys = [load_data(os.path.join(datadir, '%s_normd.json') % tp) for tp in ['train', 'val']]
+xys = [load_data(os.path.join(datadir, '%s_normd.json') % tp) for tp in ['train', 'val']]
 
 
 
@@ -50,7 +47,6 @@ def label2vec(targrtlabels:list,dims= llist.get_num()):
         if loc != -1:
             lab_vec[loc] = 1.0
     return lab_vec
-
 
 class MyDataset(Dataset):
     def __init__(self,data,maxlen=128, requires_index = False):
@@ -79,38 +75,40 @@ class MyDataset(Dataset):
 # y = [[0,1,0],[1,0,1],[1,1,0]]
 # data = zip(x,y)
 # dataset = MyDataset(data)
-# tokenizer = BertTokenizer(model_name)
+
 def collate_fn(batch):
-    text,label = [],[]
-    for d in batch:
-        text.extend(d[:2])
-        label.extend([d[2],d[2]])
-    z = tokenizer(text,return_tensors='pt',truncation=True, max_length=256,padding=True)
-    return (z.input_ids,
-            z.attention_mask,
-            z.token_type_ids,
-            torch.cat(label, 0)
-    )
+    # print(len(batch))
+    if len(batch[0]) ==2:
+        return (nn.utils.rnn.pad_sequence([x[0] for x in batch], batch_first=True),
+		    torch.stack([x[1] for x in batch], 0))
+    else:
+        return (nn.utils.rnn.pad_sequence([x[0] for x in batch], batch_first=True),
+		    torch.stack([x[1] for x in batch], 0),torch.cat([x[2] for x in batch]))
+        
 
 
-def cosent_loss(y_true,y_pred, lamb=20):
-    # print(type(y_pred))
-    # print(y_pred.shape)
-    y_true = y_true[::2]
-    #得到的y_true是(b,b)矩阵，第(i,j)元素仅当第i对句子标签0第j对句子标签1时是1，否则是0
-    y_true = (y_true[:, None] < y_true[None, :]).float()
-    y_pred = F.normalize(y_pred)
-    y_pred = torch.sum(y_pred[::2] * y_pred[1::2], dim=1) * lamb
-    # 得到的y_pred是（b,b）矩阵，第（i,j）元素是第i个句子对和第j个句子对的cosine相似度差值
-    y_pred = y_pred[:, None] - y_pred[None, :]
-    # 仅保存y_true=1即第i对句子0第j对句子1的y_pred
-    y_pred = (y_pred - (1 - y_true) * 1e12).reshape(-1)
-    y_pred = torch.cat((torch.tensor([0]).float().cuda(), y_pred), dim=0)
-    return torch.logsumexp(y_pred,dim=0)
+# (b, max_len)
+# ds_train, ds_test = MyDataset(xys[0]), MyDataset(xys[1])
+# dl_train = torch.utils.data.DataLoader(ds_train, batch_size=1, shuffle=True, collate_fn=collate_fn)
+# dl_test = torch.utils.data.DataLoader(ds_test, batch_size=1, collate_fn=collate_fn)
+# for batch in dl_train:
+def compute_kl_loss(p, q, pad_mask=None):
+    # pad_mask = (pad_mask > 0.5)
+    p_loss = F.kl_div(F.log_softmax(p,dim=-1), F.softmax(q,dim=-1), reduction='none')
+    q_loss = F.kl_div(F.log_softmax(q,dim=-1), F.softmax(p,dim=-1), reduction='none')
+    
+    # pad_mask is for seq-level tasks
+    if pad_mask is not None:
+        p_loss.masked_fill_(pad_mask, 0.)
+        q_loss.masked_fill_(pad_mask, 0.)
 
+    # You can choose whether to use function "sum" and "mean" depending on your task
+    p_loss = p_loss.mean()
+    q_loss = q_loss.mean()
 
-
-
+    loss = (p_loss + q_loss) / 2
+    return loss
+    
 
 def plot_learning_curve(record,pic_n):
     # x1 = np.arange(config['total_steps'])
@@ -147,12 +145,10 @@ def cal_hour(seconds):
     return "%d:%02d:%02d" % (h, m, s)
     
 
-def train_model(model, optimizer, train_dpath, epochs=3, train_amount=100000, batch_size=8,train_func=None, test_func=None, 
-                scheduler=None, save_file=None,accelerator=None, epoch_len=None):  # accelerator：适用于多卡的机器，epoch_len到该epoch提前停止
+def train_model(model, optimizer, train_dl, epochs=3, train_func=None, test_func=None, 
+                scheduler=None, save_file=None, accelerator=None, epoch_len=None):  # accelerator：适用于多卡的机器，epoch_len到该epoch提前停止
     best_f1 = -1
     for epoch in range(epochs):
-        train_ds = StcPairSet(sentence_pair_gen,train_amount,0.3,train_dpath,20)
-        train_dl = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
         model.train()
         print(f'\nEpoch {epoch+1} / {epochs}:')
         if accelerator:
@@ -208,29 +204,31 @@ if __name__ == '__main__':
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # device = torch.device('cpu')
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = Model(model_name).to(device)
-    ds_test = pairs_vs
-    # print('loading data completed')
-    # dl_train = torch.utils.data.DataLoader(ds_train, batch_size=32, shuffle=True, collate_fn=collate_fn)
+    model = Model(model_name, llist.get_num()).to(device)
+    ds_train, ds_test = MyDataset(xys[0]), MyDataset(xys[1])
+    print('loading data completed')
+    dl_train = torch.utils.data.DataLoader(ds_train, batch_size=32, shuffle=True, collate_fn=collate_fn)
     dl_test = torch.utils.data.DataLoader(ds_test, batch_size=32, collate_fn=collate_fn)
-    # print("dataloader completed")
+    print("dataloader completed")
     # model = Model(model_name, llist.get_num()).to(device)
     print("finish loading model")
-    mfile = './output/base4/cls_base4.pt'
+    mfile = './output/base5/256base5.pt'
 
-    epochs = 30
-    train_amount = 100000
-    batch_size = 16
-    total_steps = math.ceil(train_amount/batch_size)* epochs
+    epochs = 40
+    alpha = 4
+    total_steps = len(dl_train) * epochs
 
     optimizer, scheduler = pt_utils.get_bert_optim_and_sche(model, 1e-5, total_steps)
-    loss_func = cosent_loss
+    loss_func = nn.BCELoss()
     start_time = time.time()
     val_time = 0
     def train_func(model, ditem):
-        input_ids,attention_mask,token_type_ids, yy = ditem[0].to(device), ditem[1].to(device),ditem[2].to(device),ditem[3].to(device)
-        zz = model(input_ids,attention_mask,token_type_ids)
-        loss = loss_func(yy, zz)
+        input_ids, yy = ditem[0].to(device), ditem[1].to(device)
+        zz1 = model(input_ids)
+        zz2 = model(input_ids)
+        ce_loss = 0.5*(loss_func(zz1.float(), yy.float()) + loss_func(zz2.float(), yy.float()))
+        kl_loss = compute_kl_loss(zz1, zz2)
+        loss = ce_loss + alpha * kl_loss
         record["train_loss"].append(loss.item())
         return {'loss': loss}
 
@@ -240,40 +238,27 @@ if __name__ == '__main__':
         yt, yp = [], []
         model.eval()
         with torch.no_grad():
-            for input_ids,attention_mask,token_type_ids, yy in dl_test:
-                input_ids,attention_mask,token_type_ids = input_ids.to(device),attention_mask.to(device),token_type_ids.to(device)
-                # xx, yy = xx.to(device), yy
-                zz = model(input_ids,attention_mask,token_type_ids).detach().cpu()
-                zz = F.normalize(zz, p=2, dim=1)
-                zz = torch.sum(zz[::2] * zz[1::2], dim=1) 
-                for y in yy[::2]: yt.append(y)
-                for z in zz: yp.append((z>0.5)*1)
-            # yt = torch.stack(yt,0).numpy().astype('int64')
-            # yp = torch.stack(yp,0).numpy().astype('int64')
+            for input_ids, yy in dl_test:
+                input_ids, yy = input_ids.to(device),yy
+                zz = (model(input_ids).detach().cpu() > 0.5).float().cpu()
+                for y in yy: yt.append(y)
+                for z in zz: yp.append(z)
+            yt = torch.stack(yt,0).numpy().astype('int64')
+            yp = torch.stack(yp,0).numpy().astype('int64')
+            print(yt.shape)
             accu = metrics.accuracy_score(yt,yp)
-            prec = metrics.precision_score(yt,yp)
-            reca = metrics.recall_score(yt,yp)
-            f1 = metrics.f1_score(yt,yp)
-            # accu = sum([all(yt[i] == yp[i])*1 for i in range(len(yt))])/len(yt)
-            # # print(accu)
-            # # print(type(accu))
-            # # accu = (yt == yp).float().mean()
-            # prec = (yt + yp > 1.5).float().sum() / max(yp.sum().item(),1)
-            # reca = ((yt + yp > 1.5).float().sum() / max(yt.sum().item(),1))
-            # # f1 = 2 * prec * reca / (prec + reca)
-            # # f1 = 0 if np.isnan(f1) else f1.item()
-            # #accu,prec,reca,f1 = accu.item(),prec.item(),reca.item(),f1.item()
-            record["val_f1"].append(f1)
-            # f1_1d = metrics.f1_score(yt.unsqueeze(1).numpy().astype('int64'),yp.unsqueeze(1).numpy().astype('int64'),average='samples')
+            prec = metrics.precision_score(yt,yp,average='samples')
+            reca = metrics.recall_score(yt,yp,average='samples')
+            f1 = metrics.f1_score(yt,yp,average='samples')
         print(f'Accu: {accu:.4f},  Prec: {prec:.4f},  Reca: {reca:.4f},  F1: {f1:.5f}')
+        record['val_f1'].append(f1)
         model.train()
         t2 = time.time()
         val_time += t2-t1
         return accu,prec,reca,f1
-    train_dpath = './dataset/train_normd.json'
+
     print('Start training!')
-    # train_model(model, optimizer, '/home/qsm22/weibo_topic_recognition/dataset/train_normd.json',epochs,train_func, test_func,train_amount=train_amount,batch_size=batch_size,scheduler=scheduler, save_file=mfile)
-    train_model(model, optimizer, train_dpath, epochs, train_amount, batch_size,train_func, test_func, scheduler, save_file=mfile)
+    train_model(model, optimizer, dl_train, epochs, train_func, test_func, scheduler=scheduler, save_file=mfile)
     # plot_learning_curve(record)
     end_time = time.time()
     val_time = val_time/epochs
@@ -282,9 +267,5 @@ if __name__ == '__main__':
     train_time = total_time - val_time
     total_time,train_time,val_time = cal_hour(total_time),cal_hour(train_time),cal_hour(val_time)
     print(f'Train_time:{train_time}, Val_time:{val_time}, total_time:{total_time}')
-    plot_learning_curve(record,'./output/base4/base4_learn_curve')
+    plot_learning_curve(record,'./output/base5/256_base5')
     print('done')
-    
-# else:
-#     model.load_state_dict(torch.load(mfile), map_location=device)
-#     # model.eval()
